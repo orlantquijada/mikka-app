b462bcd6db6057aa61c826ff8b99aa022b83799f
Orlan Quijada <orlantquijada@gmail.com>
2022-02-13 05:33:02 +0800

    chore: updated api base url

--- api/axios.ts
+++ api/axios.ts
@@ -1,7 +1,8 @@
 import a from 'axios'
 
 const axios = a.create({
-  baseURL: 'http://127.0.0.1:8000/api/',
+  baseURL: 'https://secure-lowlands-89584.herokuapp.com/api/',
+  // baseURL: 'http://127.0.0.1:8000/api/',
 })
 
 export default axios
